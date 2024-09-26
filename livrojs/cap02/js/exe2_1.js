57cf4b0542b8a11c3e5019bf7636001b6b522c83
João Vitor <joaovlslusan@gmail.com>
2024-09-25 21:57:20 -0300

    horas cinema

--- livrojs/cap02/js/exe2_1.js
+++ livrojs/cap02/js/exe2_1.js
@@ -3,8 +3,10 @@ const frm = document.querySelector("form");
 const resp = document.querySelector("h3");
 
 //cria um 'ouvinte' de evento, aciona quando o botão submit for clicado 
-frm.addEventListener("submit", (e)=>{
-    const nome = frm.inNome.value; // obtem o nome digitado
-    resp.innerText = `Olá ${nome}`;// exibe a resposta do programa
-    e.preventDefault(); // vai envitar que o form envie os dados pro destino.php
-})
+frm.addEventListener("submit", executar);
+
+function executar(envento) {
+const nome = frm.inNome.value// obtem o nome digitado no form
+resp.innerHTML = `olá <i> ${nome} `;
+envento.preventDefault();
+}
