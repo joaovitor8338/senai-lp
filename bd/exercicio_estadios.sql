19399cfbbc0514e2c02ddeed5072d2238533ae42
João Vitor <joaovlslusan@gmail.com>
2024-11-05 21:48:08 -0300

    estadio_campbrazuka(terminar)

---
+++ bd/exercicio_estadios.sql
@@ -0,0 +1,42 @@
+use campeonatobrasileiro;
+
+select * from estadio;
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Maracanã','AV. Pres. Castelo Branco, Portão 3 - Maracanã, Rio de Janeiro - RJ',78838);
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value ('Allianz Parque','Av. Francisco Matarazzo, 1705 - Água Branca, São Paulo - SP', 43713);
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Arena Pantanal','Av. Agrícola Paes de Barros - Verdão, Cuiabá - MT',44000);
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Morumbi','Praça Roberto Gomes Pedrosa, 1 - Morumbi, São Paulo - SP',66795);
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Arena do Grêmio','Av. Padre Leopoldo Brentano, 110 - Humaitá, Porto Alegre - RS',55662);
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Mineirão','Av. Antônio Abrahão Caram, 1001 - Pampulha, Belo Horizonte - MG',61846);
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Estádio Nilton Santos (Engenhão)', 'R. José dos Reis, 425 - Engenho de Dentro, Rio de Janeiro - RJ',46831);
+
+insert into estadio (nome_estadio, endereco, capacidade) 
+value('Arena da Baixada (Estádio Joaquim Américo)','R. Buenos Aires, 1260 - Água Verde, Curitiba - PR', 42372);
+
+insert into estadio(nome_estadio, endereco,capacidade)
+value('Neo Química Arena','Endereço: Av. Miguel Ignácio Curi, 111 - Artur Alvim, São Paulo - SP','49205');
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Beira-Rio (Estádio José Pinheiro Borda)', 'Av.Padre.Cacique, 819 - Praia de Belas, Porto Alegre - RS', 50128);
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Arena Fonte Nova','Endereço: Ladeira da Fonte das Pedras - Nazaré, Salvador - BA','47907');
+
+insert into estadio(nome_estadio, endereco, capacidade)
+value('Maracanã','Av. Pres. Castelo Branco, Portão 3 - Maracanã, Rio de Janeiro - RJ',78838);
+
+insert into estadio(nome_estadio,endereco,capacidade)    
+value('Castelão', 'Av. Alberto Craveiro, 2901 0- Castelão, Fortaleza - CE',63903);
